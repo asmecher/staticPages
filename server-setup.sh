9b288379158adb1d268861fbc4e0f22f7c8e6ced
pmuench <philip.muench@ub.uni-osnabrueck.de>
2018-09-21 11:51:36 +0200

    connect directly to 8080

--- server-setup.sh
+++ server-setup.sh
@@ -16,5 +16,5 @@ set -xe
 
 phpenv config-add ~/ojs/lib/pkp/tools/travis/php.ini
 
-sudo socat TCP-LISTEN:80,fork,reuseaddr TCP:localhost:8080 &
+#sudo socat TCP-LISTEN:80,fork,reuseaddr TCP:localhost:8080 &
 php -S 127.0.0.1:8080 -t ~/ojs/. &

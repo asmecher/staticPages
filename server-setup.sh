e328b704bb121dda69b1b2d18fbeca8cfa723426
pmuench <philip.muench@ub.uni-osnabrueck.de>
2018-09-21 10:34:49 +0200

    and another wrong dir

--- server-setup.sh
+++ server-setup.sh
@@ -17,4 +17,4 @@ set -xe
 phpenv config-add ~/ojs/lib/pkp/tools/travis/php.ini
 
 sudo socat TCP-LISTEN:80,fork,reuseaddr TCP:localhost:8080 &
-php -S 127.0.0.1:8080 -t ojs/. &
+php -S 127.0.0.1:8080 -t ~/ojs/. &

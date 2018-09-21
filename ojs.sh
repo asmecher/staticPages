a274a66aca9106d9a68c084a63b5da7876448208
pmuench <philip.muench@ub.uni-osnabrueck.de>
2018-09-21 08:59:59 +0200

    Server-Setup

--- ojs.sh
+++ ojs.sh
@@ -7,15 +7,15 @@ echo "CREATE USER 'ojs'@'localhost' IDENTIFIED BY 'ojs'" | mysql -uroot
 echo "GRANT ALL ON ojs.* TO 'ojs'@'localhost'" | mysql -uroot
 echo "FLUSH PRIVILEGES" | mysql -uroot
 
-cd /var/www
+# cd /var/www
 
 # Clone the OJS repository
 git clone https://github.com/pkp/ojs .
 ./tools/startSubmodulesTRAVIS.sh
 cp config.TEMPLATE.inc.php config.inc.php
 mkdir ~/files
-chgrp -R www-data cache public ~/files config.inc.php
-chmod -R ug+w cache public ~/files config.inc.php
+# chgrp -R www-data cache public ~/files config.inc.php
+ä chmod -R ug+w cache public ~/files config.inc.php
 
 # Install Composer dependencies
 curl -sS https://getcomposer.org/installer | php
@@ -32,6 +32,8 @@ cd ../../..
 # Build the vue.js dependencies
 npm install
 npm run build
+
+sh server-setup.sh
 
 # Install OJS
 wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysqli&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"

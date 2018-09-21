30eeddf78a607fb8edbbc81aa1955d8a19b5399b
marher <marher@pkp-sprint-mh-ojs.pkpsprint.local>
2018-09-21 09:29:40 +0200

    Added ojs setup

--- ojs.sh
+++ ojs.sh
@@ -2,20 +2,22 @@ echo "Installing OJS"
 cd ~
 
 # Set up the OJS database
-echo "CREATE DATABASE ojs DEFAULT CHARSET utf8" | mysql -uroot -pojs
-echo "CREATE USER 'ojs'@'localhost' IDENTIFIED BY 'ojs'" | mysql -uroot -pojs
-echo "GRANT ALL ON ojs.* TO 'ojs'@'localhost'" | mysql -uroot -pojs
-echo "FLUSH PRIVILEGES" | mysql -uroot -pojs
+echo "CREATE DATABASE ojs DEFAULT CHARSET utf8" | mysql -root
+echo "CREATE USER 'ojs'@'localhost' IDENTIFIED BY 'ojs'" | mysql -uroot
+echo "GRANT ALL ON ojs.* TO 'ojs'@'localhost'" | mysql -uroot
+echo "FLUSH PRIVILEGES" | mysql -uroot
 
-cd www
+# cd /var/www
 
 # Clone the OJS repository
-git clone https://github.com/pkp/ojs .
-./tools/startSubmodulesTRAVIS.sh
+git clone https://github.com/pkp/ojs 
+cd ojs
+git submodule update --init --recursive
+# ./tools/startSubmodulesTRAVIS.sh
 cp config.TEMPLATE.inc.php config.inc.php
-mkdir ~/files
-chgrp -R www-data cache public ~/files config.inc.php
-chmod -R ug+w cache public ~/files config.inc.php
+mkdir files
+# chgrp -R www-data cache public ~/files config.inc.php
+# chmod -R ug+w cache public ~/files config.inc.php
 
 # Install Composer dependencies
 curl -sS https://getcomposer.org/installer | php
@@ -34,4 +36,5 @@ npm install
 npm run build
 
 # Install OJS
-wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=%2fhome%2fojs%2ffiles&encryption=sha1&databaseDriver=mysqli&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
+wget -O - --post-data="adminUsername=admin&adminPassword=admin&adminPassword2=admin&adminEmail=ojs@mailinator.com&locale=en_US&additionalLocales[]=en_US&clientCharset=utf-8&connectionCharset=utf8&databaseCharset=utf8&filesDir=$(pwd | sed -e 's/\//%2f/g')%2ffiles&encryption=sha1&databaseDriver=mysqli&databaseHost=localhost&databaseUsername=ojs&databasePassword=ojs&databaseName=ojs&oaiRepositoryId=ojs2.localhost" "http://localhost/ojs/index.php/index/install/install"
+
